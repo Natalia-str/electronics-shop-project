6f530cd9fce4132745650de4eccd3f29793b8833
Natalia Stroganova <ntstroganova@gmail.com>
2023-05-19 11:34:47 +0300

    correct

--- src/phone.py
+++ src/phone.py
@@ -1,7 +1,18 @@
 from item import Item
 
 class Phone(Item):
+    """
+        Класс для представления телефонов в магазине.
+        """
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
+        """
+                Создание экземпляра класса phone.
+
+                :param name: Название товара.
+                :param price: Цена за единицу товара.
+                :param quantity: Количество товара в магазине.
+                :param number_of_sim: Количество сим-карт.
+                """
         super().__init__(name, price, quantity)
         self.__number_of_sim = number_of_sim
 
