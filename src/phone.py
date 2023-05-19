2c7c98d0eca5d5bd928206bc7e3d2c9d45b89eee
Natalia Stroganova <ntstroganova@gmail.com>
2023-05-19 11:31:53 +0300

    add phone

---
+++ src/phone.py
@@ -0,0 +1,34 @@
+from item import Item
+
+class Phone(Item):
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
+        super().__init__(name, price, quantity)
+        self.__number_of_sim = number_of_sim
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, number_of_sim):
+        if number_of_sim == 0:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+        else:
+            self.__number_of_sim = number_of_sim
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.__number_of_sim})"
+
+
+
+
+    def __add__(self, other):
+        """сложение экземпляров класса Phone и Item (сложение по количеству товара в магазине)"""
+        if isinstance(other, Phone):
+            return int(self.quantity) + int(other.quantity)
+
+
+phone1 = Phone("iPhone 14", 120_000, 5, 2)
+item1 = Item("Смартфон", 10000, 20)
+print(item1 + phone1)
+phone1.number_of_sim = 0
