d5c76db2e735caceb0678a7cef9a479cc7e69c64
Natalia Stroganova <ntstroganova@gmail.com>
2023-05-20 21:18:30 +0300

    correct

--- src/phone.py
+++ src/phone.py
@@ -14,21 +14,24 @@ class Phone(Item):
                 :param number_of_sim: Количество сим-карт.
                 """
         super().__init__(name, price, quantity)
-        self.__number_of_sim = number_of_sim
-
-    @property
-    def number_of_sim(self):
-        return self.__number_of_sim
-
-    @number_of_sim.setter
-    def number_of_sim(self, number_of_sim):
-        if number_of_sim == 0:
-            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+        if number_of_sim > 0:
+            self.number_of_sim = number_of_sim
         else:
-            self.__number_of_sim = number_of_sim
+            ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+
+    # @property
+    # def number_of_sim(self):
+    #     return self.__number_of_sim
+    #
+    # @number_of_sim.setter
+    # def number_of_sim(self, number_of_sim):
+    #     if number_of_sim <= 0:
+    #         raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+    #     else:
+    #         self.__number_of_sim = number_of_sim
 
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.__number_of_sim})"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
 
 
@@ -36,10 +39,10 @@ class Phone(Item):
     def __add__(self, other):
         """сложение экземпляров класса Phone и Item (сложение по количеству товара в магазине)"""
         if isinstance(other, Phone):
-            return int(self.quantity) + int(other.quantity)
+            return self.quantity + other.quantity
 
 
-phone1 = Phone("iPhone 14", 120_000, 5, 2)
-item1 = Item("Смартфон", 10000, 20)
-print(item1 + phone1)
-phone1.number_of_sim = 0
+# phone1 = Phone("iPhone 14", 120_000, 5, 2)
+# item1 = Item("Смартфон", 10000, 20)
+# print(item1 + phone1)
+# phone1.number_of_sim = 0
