66e98dfad1a43c99214a5613db4d2cd81be8be78
Natalia Stroganova <ntstroganova@gmail.com>
2023-05-21 07:12:01 +0300

    correct

--- src/phone.py
+++ src/phone.py
@@ -19,25 +19,9 @@ class Phone(Item):
         else:
             ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
 
-    # @property
-    # def number_of_sim(self):
-    #     return self.__number_of_sim
-    #
-    # @number_of_sim.setter
-    # def number_of_sim(self, number_of_sim):
-    #     if number_of_sim <= 0:
-    #         raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
-    #     else:
-    #         self.__number_of_sim = number_of_sim
-
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
-
-    # def __add__(self, other):
-    #     """сложение экземпляров класса Phone и Item (сложение по количеству товара в магазине)"""
-    #     if isinstance(other, Item):
-    #         return self.quantity + other.quantity
 
 
 # phone1 = Phone("iPhone 14", 120_000, 5, 2)
