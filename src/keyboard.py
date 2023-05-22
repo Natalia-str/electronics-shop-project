801f7da4756df040026c4ef76d00c1a525b51a95
Natalia Stroganova <ntstroganova@gmail.com>
2023-05-22 23:40:08 +0300

    correct Keyboard

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,28 +1,21 @@
 from src.item import Item
+from src.mixinlang import MixinLang
 
 class Keyboard(Item, MixinLang):
-    def __init__(self, name: str, price: float, quantity: int, language: str) -> None:
+    def __init__(self, name: str, price: float, quantity: int) -> None:
         """
             Создание экземпляра класса keyboard.
 
             :param name: Название товара.
             :param price: Цена за единицу товара.
             :param quantity: Количество товара в магазине.
-            :param language: Язык раскладки.
             """
         super().__init__(name, price, quantity)
-        self.__language = language
 
-
-class MixinLang:
-    language = "EN"
-    def change_lang(self, language):
-        if language == "EN":
-            language = "RU"
-            return language
-        elif language == "RU":
-            language = "EN"
-            return language
-        else:
-            raise AttributeError("property 'language' of 'KeyBoard' object has no setter")
-
+# kb = Keyboard('Dark Project KD87A', 9600, 5)
+# print(str(kb))
+# print(str(kb.language))
+# kb.change_lang()
+# print(str(kb.language))
+# kb.change_lang().change_lang()
+# print(str(kb.language))
